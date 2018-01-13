5214ae5ffb3e7af860a99337a68c1e446122cb30
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2018-01-13 02:11:26 +0100

    no longer need CSV dependency

--- Package.swift
+++ Package.swift
@@ -9,13 +9,12 @@ let dependencies:[Package.Dependency] = [
     .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.0"),
     .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
     .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.8.4"),
-    .package(url: "https://github.com/krzyzanowskim/Kitura-CredentialsGitHub.git", .branch("master")), // Fix dependency chain
-    .package(url: "https://github.com/yaslab/CSV.swift", from: "2.1.0")
+    .package(url: "https://github.com/krzyzanowskim/Kitura-CredentialsGitHub.git", .branch("master")) // Fix dependency chain
 ]
 
 var targets:[Target] = [
     .target(name: "PlaygroundServer", dependencies: [ .target(name: "Application"), .target(name: "OnlinePlayground"), "Kitura" , "HeliumLogger"]),
-    .target(name: "Application", dependencies: [ "Kitura", "Kitura-WebSocket", "Configuration", "CloudEnvironment", "Utility", "CredentialsGitHub", "KituraStencil", "CSV"], exclude: ["DerivedData","public","node_modules"]),
+    .target(name: "Application", dependencies: [ "Kitura", "Kitura-WebSocket", "Configuration", "CloudEnvironment", "Utility", "CredentialsGitHub", "KituraStencil"], exclude: ["DerivedData","static","node_modules"]),
     .target(name: "OnlinePlayground"),
     .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
 ]
