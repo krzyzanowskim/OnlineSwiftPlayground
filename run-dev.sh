9ea64e4011b92703d3daf5ab3a74a6e7d1d9d569
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2018-03-28 11:26:32 +0200

    Add convenience run-dev.sh script

---
+++ run-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+if [ $(program_is_installed xcrun) == 1 ]; then
+    xcrun swift build -c debug && xcrun swift run -c debug
+else
+    swift build -c debug && swift run -c debug    
+fi
+
+if [ $(program_is_installed npm) == 1 ]; then
+    $(npm bin)/webpack -w
+fi
+
