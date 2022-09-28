8c8cbd506e868ab3c8c87b77aa9635083c33b1ad
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2022-09-29 00:54:26 +0200

    Add SSL configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,8 +1,14 @@
 import Vapor
 import Leaf
+import NIOSSL
 
 public extension Application {
     func configure() throws {
+        try http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+            certificateChain: NIOSSLCertificate.fromPEMFile(directory.workingDirectory + "/ssl/certificate.crt").map { .certificate($0) },
+            privateKey: .privateKey(.init(file: directory.workingDirectory + "/ssl/private.key", format: .pem))
+        )
+
         middleware.use(NotFoundMiddleware())
         middleware.use(FileMiddleware(publicDirectory: directory.publicDirectory))
         middleware.use(sessions.middleware)
